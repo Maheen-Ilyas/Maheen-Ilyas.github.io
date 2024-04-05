dc4e1a2ede64ee72c7c254c614c76c9c3fdd84c2
Maheen Ilyas <mahilyas05@gmail.com>
2024-04-05 17:08:49 +0530

    Home and About section done, links to be added

---
+++ app.js
@@ -0,0 +1,23 @@
+const cursorDot = document.querySelector("[data-cursor-dot]");
+const cursorOutline = document.querySelector("[data-cursor-outline]");
+
+window.addEventListener("mousemove", function (e) {
+  positionX = e.clientX;
+  positionY = e.clientY;
+
+  cursorDot.style.left = `${positionX}px`;
+  cursorDot.style.top = `${positionY}px`;
+
+  cursorOutline.animate(
+    { left: `${positionX}px`, top: `${positionY}px` },
+    { duration: 500, fill: "forwards" }
+  );
+});
+
+const menuIcon = document.querySelector(".menu-icon");
+const menuItems = document.querySelector(".menu-items");
+
+function toggleMenu() {
+  menuItems.classList.toggle("open");
+  menuIcon.classList.toggle("open");
+}
